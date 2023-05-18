22127a59fd14fd960426c23858a1b84f43623064
ZhenianaKozhan <94985681+ZhenianaKozhan@users.noreply.github.com>
2023-05-18 20:21:27 +0300

    add login form and styles for it

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -19,18 +19,54 @@ export const LoginForm = () => {
       <div className='loginFormContainer'>
         <div className='loginElementContainer'>
           <div className='logoInsta' role='button'></div>
-          <div className='loginForm'>
-            <form onSubmit={handleSubmit(onSubmit)}>
-              <input {...register('exampleRequired', { required: true })} />
-              <input {...register('exampleRequired', { required: true })} />
+          <div className='loginFormBox'>
+            <form className='loginForm' onSubmit={handleSubmit(onSubmit)}>
+              <input
+                className='loginInput'
+                placeholder='Номер телефону або електронна пошта'
+                {...register('login', { required: true })}
+              />
+              <input
+                className='loginInput'
+                placeholder='Пароль'
+                {...register('password', { required: true })}
+              />
               {errors.exampleRequired && <span>This field is required</span>}
-              <input type='submit' />
+              <input className='formSubmitButton' type='submit' />
             </form>
-            <div className='loginElementOr'> АБО </div>
+            <p className='loginElementOr'> або </p>
+            <a className='facebookLinkAuth' href='/'>
+              Увійти через Facebook
+            </a>
+          </div>
+          <a href='/' style={{ marginTop: '12px', color: 'rgb(0, 149, 246)' }}>
+            Забули пароль?
+          </a>
+        </div>
+        <div className='registerLinkContainer'>
+          Не маєте облікового запису? <a href='/'>Зареєструйтеся</a>
+        </div>
+        <div className='downloadBox'>
+          <p>Завантажити додаток</p>
+          <div className='downloadBoxLink'>
+            <a href='/'>
+              <img
+                alt='play market link'
+                height={40}
+                src='https://static.cdninstagram.com/rsrc.php/v3/yq/r/54pQonhlyEd.png'
+                width={135}
+              />
+            </a>
+            <a href='/'>
+              <img
+                alt='microsoft link'
+                height={40}
+                src='https://static.cdninstagram.com/rsrc.php/v3/yx/r/0WRk16YTMoq.png'
+                width={111}
+              />
+            </a>
           </div>
         </div>
-        <div className='loginElementContainer'>Не маєте облікового запису? Зареєструйтеся</div>
-        <div></div>
       </div>
     </article>
   );
